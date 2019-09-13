44df5c382b000fd38627ef5f24906ba0a75fbd4c
markoneill6 <mhboneill@hotmail.com>
2019-09-13 22:45:09 +0100

    more layout improvements but nav bar not working in small screen

--- app/assets/Javascripts/application.js
+++ app/assets/Javascripts/application.js
@@ -2,4 +2,4 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
-//= require_tree
+// //= require_tree
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require turbolinks
-//= require_tree
