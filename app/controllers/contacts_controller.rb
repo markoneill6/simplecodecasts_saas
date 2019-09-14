e1e95c5a42ced0fdfc566338ab2dc5fb3b0d4263
markoneill6 <mhboneill@hotmail.com>
2019-09-14 22:49:27 +0100

    Adding contacts form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,28 @@
 class ContactsController < ApplicationController
-  def new
+  def new # this is the form called after the get action redirection from the index !
+    @contact = Contact.new
   end
 
-  def create
+  def create # this is coming from the post action !
+    @contact = Contact.new(contact_params)
+    if @contact.save #committing into the database
+      name = params[:contact][:name] # from here, getting the data from the db to be sent by the email below...
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      ContactMailer.contact_mail(name, email, body).deliver # ... sending data to our email
+      flash[:success] = "Message sent."
+      redirect_to root_path
+      #redirect_to new_contact_path, notice: "Message sent." # OJO: redirection is into contact_new...
+    else
+      flash[:danger] = "Error ocurred, message not sent."
+      redirect_to new_contact_path
+      #redirect_to new_contact_path, notice: "Error occured."
+    end
+
+  end
+
+  private # a new, important addition to rails ! security !!
+  def contact_params # "white-listing" parameters...
+    params.require(:contact).permit(:name, :email, :comments)
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-
+  # one of the forms of validation, see rails guide for more info...
+  validates :email, presence: true
+  validates :name, presence: true
 end
