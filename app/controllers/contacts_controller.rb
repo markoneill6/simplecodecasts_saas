13427073f9a17d05bf856f74db4784daab6e6a1d
markoneill6 <mhboneill@hotmail.com>
2019-09-16 17:13:17 +0100

    Saving form data and error handling

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,26 +3,45 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
-  def create # this is coming from the post action !
-    @contact = Contact.new(contact_params)
-    if @contact.save #committing into the database
-      name = params[:contact][:name] # from here, getting the data from the db to be sent by the email below...
-      email = params[:contact][:email]
-      body = params[:contact][:comments]
-      ContactMailer.contact_mail(name, email, body).deliver # ... sending data to our email
-      flash[:success] = "Message sent."
-      redirect_to root_path
+  # Commenting out copied code from Github (rows 7 to 28)
+  # def create # this is coming from the post action !
+   # @contact = Contact.new(contact_params)
+    #if @contact.save #committing into the database
+     # name = params[:contact][:name] # from here, getting the data from the db to be sent by the email below...
+     # email = params[:contact][:email]
+     # body = params[:contact][:comments]
+     # ContactMailer.contact_mail(name, email, body).deliver # ... sending data to our email
+     # flash[:success] = "Message sent."
+     # redirect_to root_path
       #redirect_to new_contact_path, notice: "Message sent." # OJO: redirection is into contact_new...
-    else
-      flash[:danger] = "Error ocurred, message not sent."
-      redirect_to new_contact_path
-      #redirect_to new_contact_path, notice: "Error occured."
-    end
+   # else
+    #  flash[:danger] = "Error ocurred, message not sent."
+     # redirect_to new_contact_path
+     # #redirect_to new_contact_path, notice: "Error occured."
+   # end
 
+#  end
+
+#  private # a new, important addition to rails ! security !!
+#  def contact_params # "white-listing" parameters...
+#    params.require(:contact).permit(:name, :email, :comments)
+#  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      flash[:success] = 'Message Sent.'
+      redirect_to new_contact_path
+    else
+      flash[:error] = 'Error occurred, message has not been sent'
+      redirect_to new_contact_path
+    end
   end
 
-  private # a new, important addition to rails ! security !!
-  def contact_params # "white-listing" parameters...
+  private
+  def contact_params
     params.require(:contact).permit(:name, :email, :comments)
   end
+
 end
