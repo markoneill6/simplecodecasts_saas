14e6974408db7cc823637bee1f4dacb8e9f98120
markoneill6 <mhboneill@hotmail.com>
2019-09-27 23:37:23 +0100

    Added error handling to users.js

--- app/assets/Javascripts/users.js
+++ app/assets/Javascripts/users.js
@@ -1,38 +1,40 @@
 $(document).ready(function() {
     Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-    // Watch for a form submission:
-    $("#form-submit-btn").click(function(event) {
+
+    $('#form-submit-btn').click(function(event) {
         event.preventDefault();
         $('input[type=submit]').prop('disabled', true);
         var error = false;
-        var ccNum = $('#card_number').val(),
-            cvcNum = $('#card_code').val(),
-            expMonth = $('#card_month').val(),
-            expYear = $('#card_year').val();
+        // var ccNum = $('#card_number').val(),
+        //     cvcNum = $('card_code').val(),
+        //     expMonth = $('card_month').val(),
+        //     expYear = $('card_year').val();
+        //     console.log(ccNum);
 
         if (!error) {
-            // Get the Stripe token:
-            Stripe.createToken({
-                number: ccNum,
-                cvc: cvcNum,
-                exp_month: expMonth,
-                exp_year: expYear
-            }, stripeResponseHandler);
+            Stripe.card.createToken($('#new_user'), stripeResponseHandler);
         }
         return false;
-    }); // form submission
+    }); //form submission
 
-    function stripeResponseHandler(status, response) {
-        // Get a reference to the form:
-        var f = $("#new_user");
+    function stripeResponseHandler(status, response){
+        if (status == 200){
+            //Get a reference to the form
+            var f = $('#new_user');
 
-        // Get the token from the response:
-        var token = response.id;
+            //Get the token from the response
+            var token = response.id;
+            console.log("Token:" + token);
 
-        // Add the token to the form:
-        f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
+            //Add the token to the form
+            f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
 
-        // Submit the form:
-        f.get(0).submit();
+            //Submit the form
+            f.get(0).submit();
+        }
+        else {
+            $('#stripe_error').text(response.error.message).show();
+            $('input[type=submit]').attr('disabled', false)
+        }
     }
 });
