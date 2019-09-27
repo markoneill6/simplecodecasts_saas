f8e9c43c2b73d9aa3950e4cb9ce1e1f9f2df4235
markoneill6 <mhboneill@hotmail.com>
2019-09-27 23:07:46 +0100

    Adde before_action to application_controller file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  #before_action :authenticate_user!
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:stripe_card_token])
   end
 end
